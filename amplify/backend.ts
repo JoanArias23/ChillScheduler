1f4b2069456fa3518dbcb2169fe8d497e7976106
Joan Arias <joanarias24@gmail.com>
2025-08-14 18:07:21 -0500

    Comment out all Lambda permissions and environment variables
    - Remove all IAM policy statements
    - Remove all environment variable assignments
    - Remove all service principal grants
    - Keep only basic backend definition
    
    This removes all infrastructure dependencies that could cause
    deployment failures. The Lambdas are now completely isolated.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,7 +3,6 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { executeJob } from './functions/executeJob/resource';
 import { scheduleManager } from './functions/scheduleManager/resource';
-import { Policy, PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -15,59 +14,60 @@ const backend = defineBackend({
   scheduleManager,
 });
 
-// Grant the Lambda function permissions to access DynamoDB tables
-const jobsTableArn = backend.data.resources.tables['Job'].tableArn;
-const executionsTableArn = backend.data.resources.tables['JobExecution'].tableArn;
+// Commenting out all permissions and environment variables for now
+// to isolate deployment issues
 
-// Environment variables will be hardcoded in the Lambda for now
+// // Grant the Lambda function permissions to access DynamoDB tables
+// const jobsTableArn = backend.data.resources.tables['Job'].tableArn;
+// const executionsTableArn = backend.data.resources.tables['JobExecution'].tableArn;
 
-// Add permissions for executeJob
-backend.executeJob.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    actions: [
-      'dynamodb:GetItem',
-      'dynamodb:PutItem',
-      'dynamodb:UpdateItem',
-      'dynamodb:Query'
-    ],
-    resources: [jobsTableArn, executionsTableArn],
-  })
-);
+// // Add permissions for executeJob
+// backend.executeJob.resources.lambda.addToRolePolicy(
+//   new PolicyStatement({
+//     actions: [
+//       'dynamodb:GetItem',
+//       'dynamodb:PutItem',
+//       'dynamodb:UpdateItem',
+//       'dynamodb:Query'
+//     ],
+//     resources: [jobsTableArn, executionsTableArn],
+//   })
+// );
 
-// Add permissions for scheduleManager
-backend.scheduleManager.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    actions: [
-      'events:PutRule',
-      'events:PutTargets',
-      'events:DeleteRule',
-      'events:RemoveTargets',
-      'events:DescribeRule',
-      'lambda:AddPermission',
-      'lambda:RemovePermission'
-    ],
-    resources: ['*'],
-  })
-);
+// // Add permissions for scheduleManager
+// backend.scheduleManager.resources.lambda.addToRolePolicy(
+//   new PolicyStatement({
+//     actions: [
+//       'events:PutRule',
+//       'events:PutTargets',
+//       'events:DeleteRule',
+//       'events:RemoveTargets',
+//       'events:DescribeRule',
+//       'lambda:AddPermission',
+//       'lambda:RemovePermission'
+//     ],
+//     resources: ['*'],
+//   })
+// );
 
-backend.scheduleManager.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    actions: [
-      'dynamodb:UpdateItem',
-      'dynamodb:GetItem'
-    ],
-    resources: [jobsTableArn],
-  })
-);
+// backend.scheduleManager.resources.lambda.addToRolePolicy(
+//   new PolicyStatement({
+//     actions: [
+//       'dynamodb:UpdateItem',
+//       'dynamodb:GetItem'
+//     ],
+//     resources: [jobsTableArn],
+//   })
+// );
 
-// Add environment variables for scheduleManager
-backend.scheduleManager.addEnvironment('EXECUTE_JOB_ARN', backend.executeJob.resources.lambda.functionArn);
-backend.scheduleManager.addEnvironment('JOBS_TABLE', backend.data.resources.tables['Job'].tableName);
+// // Add environment variables for scheduleManager
+// backend.scheduleManager.addEnvironment('EXECUTE_JOB_ARN', backend.executeJob.resources.lambda.functionArn);
+// backend.scheduleManager.addEnvironment('JOBS_TABLE', backend.data.resources.tables['Job'].tableName);
 
-// Add environment variables for executeJob
-backend.executeJob.addEnvironment('JOBS_TABLE', backend.data.resources.tables['Job'].tableName);
-backend.executeJob.addEnvironment('EXECUTIONS_TABLE', backend.data.resources.tables['JobExecution'].tableName);
-backend.executeJob.addEnvironment('CLAUDE_API_URL', 'http://claude.chinchilla-ai.com:3000/query');
+// // Add environment variables for executeJob
+// backend.executeJob.addEnvironment('JOBS_TABLE', backend.data.resources.tables['Job'].tableName);
+// backend.executeJob.addEnvironment('EXECUTIONS_TABLE', backend.data.resources.tables['JobExecution'].tableName);
+// backend.executeJob.addEnvironment('CLAUDE_API_URL', 'http://claude.chinchilla-ai.com:3000/query');
 
-// Allow EventBridge to invoke executeJob Lambda
-backend.executeJob.resources.lambda.grantInvoke(new ServicePrincipal('events.amazonaws.com'));
+// // Allow EventBridge to invoke executeJob Lambda
+// backend.executeJob.resources.lambda.grantInvoke(new ServicePrincipal('events.amazonaws.com'));
